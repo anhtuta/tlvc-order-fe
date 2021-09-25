45f10bd6ee4776bc10cbfb766a80f892c3edc44e
Anh Tú Tạ <anhtu51195@gmail.com>
2021-09-26 01:05:24 +0700

    Minor update checkbox

--- src/components/Input/Checkbox.js
+++ src/components/Input/Checkbox.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Checkbox.scss';
 
+/**
+ * Ref: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_custom_checkbox
+ */
 const Checkbox = (props) => {
   const { text, name, className = '', checked = false, disabled = false } = props;
   const classes = `cb ${className}`;
 
   const onChange = (e) => {
-    props.onChange(e.target.name, e.target.checked);
+    props.onChange(props.name, e.target.checked);
   };
 
   return (
@@ -26,7 +29,11 @@ const Checkbox = (props) => {
   );
 };
 
-// Note: HTML thì ko cần onChange, nếu click vào checkbox thì nó tự thay đổi
-// (chuyển từ chưa check sang check và ngược lại). Nhưng với React thì ko có
-// thì lúc click sẽ ko thay đổi gì!
+/**
+ * Note: HTML thì ko cần onChange, nếu click vào checkbox thì nó tự thay đổi
+ * (chuyển từ chưa check sang check và ngược lại). Nhưng với React thì ko có
+ * thì lúc click sẽ ko thay đổi gì!
+ * Note2: class cha dùng component này phải truyền props checked = true/false,
+ * nếu ko sẽ ko hoạt động!
+ */
 export default Checkbox;
